17c32d49b5dfa7e146eb8a1836bce7b0839de621
Francisco Amador De Jesus Luis <framador@c2r4s10.42lisboa.com>
2025-05-31 11:08:47 +0100

    42 update: 11:08:39 31-05-2025
    Fixed ref small mistake

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: framador <framador@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 18:06:19 by framador          #+#    #+#             */
-/*   Updated: 2025/03/12 19:22:22 by framador         ###   ########.fr       */
+/*   Updated: 2025/05/31 11:07:18 by framador         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(void)
 
 	stringPTR = &brain;
 	std::cout << "address of brain:" << (long)&brain << std::endl;
-	std::cout << "address of strPTR:" << (long)&stringPTR << std::endl;
+	std::cout << "address of strPTR:" << (long)stringPTR << std::endl;
 	std::cout << "address of strREF:" << (long)&stringREF << std::endl;
 
 	std::cout << "brain contains:" << brain << std::endl;
